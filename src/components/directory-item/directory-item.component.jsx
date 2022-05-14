f96b674c1a610d867dccab81147b74d02ab95907
Anant Gangwar <gangwaranant@gmail.com>
2022-05-14 23:25:18 +0530

    added Navigation to categories on Home Page

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,4 +1,7 @@
 // import "./directory-item.styles.scss";
+
+import { useNavigate } from "react-router-dom";
+
 import {
   BackgroundImage,
   Body,
@@ -6,9 +9,13 @@ import {
 } from "./directory-item.styles";
 
 const DirectoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(route);
+
   return (
-    <DirectoryItemContainer>
+    <DirectoryItemContainer onClick={onNavigateHandler}>
       <BackgroundImage
         imageUrl={imageUrl}
         // style={{
